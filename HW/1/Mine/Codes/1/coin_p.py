8a0540fc8d49af44db158dbb327099b75a0da6c9
Ali Nazari <ali.nazari@aut.ac.ir>
2018-11-07 19:56:39 +0330

    complete Q1 source code

--- HW/1/Mine/Codes/1/coin_p.py
+++ HW/1/Mine/Codes/1/coin_p.py
@@ -5,17 +5,17 @@ from matplotlib import pyplot
 def simulate(flip_number, h_probability):
     count = 0
     point_list = []
-    for i in range(1, flip_number):
+    for i in range(0, flip_number):
         if random.random() < h_probability:
             count += 1
         point_list.append(count)
-        if i == 50 or i == 100 or i == 500 or i == 1000:
+        if i == 50 or i == 100 or i == 500:
             print('Number of heads in round ' + str(i) + ' = ' + str(count))
 
     pyplot.title('H Probability = ' + str(h_probability))
-    print('Number of heads at the End: ' + str(count))
+    print('Number of heads in round 1000 = ' + str(count))
 
-    pyplot.plot(range(1, flip_number), point_list)
+    pyplot.plot(range(0, flip_number), point_list)
     pyplot.axis([0, 1000, 0, 1000])
 
 
