0f44d485378cded7bb30a5d56ac3ebb25f10540b
Ali Nazari <ali.nazari@aut.ac.ir>
2018-11-09 18:14:04 +0330

    calculate sum of columns and probability of each cell

--- HW/1/Mine/Codes/2/process_data.py
+++ HW/1/Mine/Codes/2/process_data.py
@@ -1,6 +1,15 @@
+import numpy
 import pandas
 
 data_set_train = pandas.read_csv('train.csv', header=None)
 data_set_test = pandas.read_csv('test.csv', header=None)
 
-print(data_set_train)
+sum_columns = data_set_train.sum(0)
+probability = numpy.empty((1600, 1593))
+
+for i in range(1593):
+    for j in range(1600):
+        probability[j][i] = data_set_train.values[j, i] / float(sum_columns[i])
+
+
+print(probability[0][6])
