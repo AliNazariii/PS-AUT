3182c4d608414913c1244d15d419b49b8bf6a672
Ali Nazari <ali.nazari@aut.ac.ir>
2018-11-10 17:51:12 +0330

    calculate probability of each class

--- HW/1/Mine/Codes/2/process_data.py
+++ HW/1/Mine/Codes/2/process_data.py
@@ -1,15 +1,36 @@
 import numpy
 import pandas
 
+
+def find_class_probabilities(data_train):
+    p_a = 0.0
+    p_b = 0.0
+
+    for p in range(1600):
+        if data_train.values[p, 1593] == 1:
+            p_a += 1.0
+        elif data_train.values[p, 1593] == -1:
+            p_b += 1.0
+
+    p_a = p_a / 1600
+    p_b = p_b / 1600
+
+    return p_a, p_b
+
+
 data_set_train = pandas.read_csv('train.csv', header=None)
 data_set_test = pandas.read_csv('test.csv', header=None)
 
-sum_columns = data_set_train.sum(0)
-probability = numpy.empty((1600, 1593))
+# sum_columns = data_set_train.sum(0)
+# probability = numpy.empty((1600, 1593))
+#
+# for i in range(1593):
+#     for j in range(1600):
+#         probability[j][i] = data_set_train.values[j, i] / float(sum_columns[i])
+#
+#
+# print(probability[0][6])
 
-for i in range(1593):
-    for j in range(1600):
-        probability[j][i] = data_set_train.values[j, i] / float(sum_columns[i])
+probability_a, probability_b = find_class_probabilities(data_set_train)
 
-
-print(probability[0][6])
+print(probability_a, probability_b)
